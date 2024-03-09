b0ca3fddacbff9be185ddbceab68b49c3b6f4a92
AbhishekSingh <siyanisabhishek@gmail.com>
2024-03-09 09:35:34 +0530

    fix: route base url fix

--- lib/axios_instance.ts
+++ lib/axios_instance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://pencil-io.vercel.app/",
   timeout: 8000,
   headers: {
     Accept: "application/json",
